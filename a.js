2255b185693b7f9060c63dd60ee46795102feffa
frojoo <wndyd29605@gmail.com>
2023-04-03 14:53:15 +0900

    calendar

--- a.js
+++ a.js
@@ -1,12 +1,27 @@
-const myProfile = {
-  nickName: "JY",
-  age: 28,
-  job: "student",
-  isGenius: true,
-};
+// const fruits = ["apple", "banana", "orange", "kiwi", "strawberry"];
 
-const { nickName, age } = myProfile;
+// for (let i = 0; i < fruits.length; i++) {
+//   console.log(fruits[i]);
+// }
 
-console.log(nickName);
-console.log(myProfile.nickName);
-console.log(age);
+// fruits.map((v, i) => {
+//   console.log(`${i + 1}번째 과일: ${v}`);
+// });
+
+const array = [1, 2, 4, 8];
+
+console.log(array);
+
+const squareArray = array.map((v) => v * v);
+
+const squareArray2 = [];
+for (let i = 0; i < array.length; i++) {
+  squareArray2.push(array[i] * array[i]);
+}
+
+console.log(squareArray);
+console.log(squareArray2);
+
+let age = 18;
+let beverage = age >= 20 ? "Beer" : "Coke";
+console.log(beverage);
--- src/fakeHooks/index.jsx
+++
@@ -1,7 +0,0 @@
-export const fakeUseState = () => {
-  let value;
-  const setValue = () => {};
-
-  //   return { value: value, setValue: setValue };
-  return { value, setValue };
-};
